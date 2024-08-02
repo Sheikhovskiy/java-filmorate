37c2b6fe838f18788804985b6215bf27d79b79a0
Sheikh <benabdallah@bk.ru>
2024-08-02 16:29:19 +0300

    Скомпилировать проект

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApp.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApp.java
@@ -1,13 +1,13 @@
-//package ru.yandex.practicum.filmorate;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//
-//@SpringBootApplication
-//public class FilmorateApp {
-//    public static void main(String[] args) {
-//
-//        SpringApplication.run(FilmorateApp.class, args);
-//    }
-//}
+package ru.yandex.practicum.filmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FilmorateApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(FilmorateApp.class, args);
+    }
+}
