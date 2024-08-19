9972b09a7379b47349e5d61ebb5c9d0112954bc3
Sheikh <benabdallah@bk.ru>
2024-08-20 02:46:05 +0300

    Внести правки по чекстайлу спринт №11 часть 4

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
