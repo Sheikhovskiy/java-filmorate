ee715f58dbc44b7ed1e898811838d97c26eb822b
Sheikh <benabdallah@bk.ru>
2024-08-28 08:57:36 +0300

    Фикс checkstyle по ТЗ 12 №9

--- src/main/java/ru/yandex/practicum/filmorate/repository/JdbcMpaRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/JdbcMpaRepository.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import java.sql.ResultSet;
 
 
 import java.util.List;
